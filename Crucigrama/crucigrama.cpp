d219c683de7fbe5d39ed43abb5afa095989d3bbc
Julian Vidal <jvidal@munidigital.com>
2026-07-23 19:56:34 -0300

    avanzada implementacion de crucigrama

--- Crucigrama/crucigrama.cpp
+++ Crucigrama/crucigrama.cpp
@@ -1,21 +1,35 @@
 #include <iostream>
+#include <string>
 #include <map>
 
 using namespace std;
 
 /// funtions prototypes
 void menu();
+void showMatrix(string matrix [14][15]);
 //void showOptions(int);
 
 void menu(){
-	cout<<"Que desea MOSTRAR?\n";
-	cout<<"0 = Crucigrama\n";
+	cout<<"\nQue desea MOSTRAR?\n\n";
 	cout<<"1 = Pista 1\n";
 	cout<<"2 = Pista 2\n";
 	cout<<"3 = Pista 3\n";
 	cout<<"4 = Pista 4\n";
 	cout<<"5 = Pista 5\n\n";
+	cout<<"Ingrese opcion: ";
 }
+	
+void showMatrix(string matrix [14][15]){
+	
+	for(int i = 0; i <14; i ++){
+		for (int j = 0; j <15; j ++){
+			cout<< matrix[i][j] << " ";
+		}
+		cout<< endl;
+	}
+	
+}
+
 	
 /*
 void showOptions(int){
@@ -43,25 +57,61 @@ void showOptions(int){
 
 int main(int argc, char *argv[]) {
 	
-	/// maps initialization
-	map<int, string> palabras = {
-		{1, "BENNINGTON"}, {2, "TORTITAS"}, {3, "MESSI"}, {4, "TANGO"}, {5, "WOLVERINE"}
+	/// maps initializations
+	map<string, string> palabras = {
+		{"1", "BENNINGTON"},
+		{"2", "TORTITAS"},
+		{"3", "MESSI"},
+		{"4", "TANGO"},
+		{"5", "WOLVERINE"}
 	};
 	
-	map<int, string> pistas = {
-		{1, "Cantante de la cancion NUMB"}, {2, "Viste cuando decis, que ganas de tomar un buen mate con ..."}, {3, "D10S"}, {4, "Baile Argentino"}, {5, "El X Men mas conocido"}
+	map<string, string> pistas = {
+		{"1", "1: Cantante de la cancion \"NUMB\""}, 
+		{"2", "2: Viste cuando decis, que ganas de tomar un buen mate con ..."}, 
+		{"3", "3: D10S"}, 
+		{"4", "4: Baile Argentino"}, 
+		{"5", "5: El X Men mas conocido"}
 	};
 	/// end maps
 	
+	/// matriz inicialization
+	string matrix[14][15] = {
+		{" ", " ", "5", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " "},
+		{" ", " ", "#", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " "},
+		{" ", " ", "#", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " "},
+		{" ", " ", "#", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " "},
+		{" ", " ", "#", " ", " ", " ", " ", " ", " ", "2", " ", " ", " ", " ", " "},
+		{" ", " ", "#", " ", " ", " ", " ", " ", " ", "#", " ", " ", " ", " ", " "},
+		{"1", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", " ", " ", " ", " "},
+		{" ", " ", "#", " ", " ", " ", " ", " ", " ", "#", " ", " ", " ", " ", " "},
+		{" ", " ", "#", " ", " ", " ", " ", " ", "4", "#", "#", "#", "#", "#", " "},
+		{" ", " ", "#", " ", " ", " ", " ", " ", " ", "#", " ", " ", " ", " ", " "},
+		{" ", " ", "#", " ", " ", " ", " ", " ", " ", "#", " ", " ", " ", " ", " "},
+		{" ", " ", " ", " ", " ", " ", " ", " ", " ", "#", " ", " ", " ", " ", " "},
+		{" ", " ", " ", " ", " ", "3", "#", "#", "#", "#", "#", " ", " ", " ", " "},
+		{" ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " "}
+	};
+	/// end matriz
+	
 	bool terminated = false;
-	int option;
+	string option;
 	
 	do{
+		
 		menu();
-		cin>>option;
-		cout<<"\n";
-		cout<<pistas[option];
-		cout<<"\n\n";
+		cin >> option;
+		cout << "\n";
+		
+		if (palabras.count(option) <= 0) {
+			cout << "Opción inválida! Reintente" << endl;
+			continue;
+		}
+		
+		cout << pistas[option];
+		cout << "\n\n";
+		showMatrix(matrix);
+		
 	} while(!terminated); // !terminated == not terminated == terminated!=true
 	
 	return 0;
